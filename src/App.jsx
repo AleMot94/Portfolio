c1f8682e341771e4cecadfc0ffc7cd0eb9d319e4
AleMot94 <Alector666@gmai.com>
2023-06-23 15:16:59 -0300

    boton dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,34 @@ import '@fontsource/roboto/500.css';
 import { NavBar } from './components/NavBar';
 import { Main } from './components/Main';
 
+
+import { useState } from 'react';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
 function App() {
 
+    const [ dark, setDark] = useState(true)
+
+    const theme = createTheme({
+      palette: {
+        mode: dark ? "light" : "dark"
+      }
+    })
+
+    const handleChange = (event) => {
+      setDark(event.target.checked);
+    };
+
   return (
+    <ThemeProvider theme={theme}>
     <div>
       <NavBar/>
+      <FormControlLabel control={<Switch checked={dark} onChange={handleChange}/>} label={dark ? "light" : "dark"} />
       <Main/>
     </div>
+    </ThemeProvider>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,28 +7,32 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
+
 export const NavBar = () => {
+
+    
+
     return(
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Logo React
-            </Typography>
-            <Button color="inherit">Acerca de Mi</Button>
-            <Button color="inherit">Trabajos</Button>
-            <Button color="inherit">Contacto</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
+          <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Logo React
+              </Typography>
+              <Button color="inherit">Acerca de Mi</Button>
+              <Button color="inherit">Trabajos</Button>
+              <Button color="inherit">Contacto</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
     )
 }
